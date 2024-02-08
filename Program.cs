463923fc8e97f28aa8f10ec69a8367774507b205
Baltazzzzar <dariok@student.uia.no>
2024-02-08 17:38:58 +0100

    functions and test

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double ConvertToCm(int inches)
+{
+    return inches * 2.54;
+}
+
+double ConvertToM(int inches)
+{
+    return ConvertToCm(inches) / 100;
+}
+
+double ConvertToMm(int inches)
+{
+    return ConvertToCm(inches) * 10;
+}
+
+Test(ConvertToCm(1) == 2.54, "1 inch is 2.54 cm");
+Test(ConvertToM(1) == 0.0254, "1 inch is 0.0254 cm");
+Test(ConvertToMm(1) == 25.4, "1 inch is 25.4 cm");
+
+void Test(bool criteria, string description)
+{
+    if (criteria)
+    {
+        Console.WriteLine($"SUCCESS, {description}");
+    }
+    else
+    {
+        Console.WriteLine($"FAILURE, {description}");
+    }
+}
